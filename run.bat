8e46b9e96a9ba698fbab38de1b8c30642570edfd
lykhorovych <oleglihorovich@gmail.com>
2023-06-07 23:30:05 +0300

    v4

--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
 rem python -m venv venv
 rem cd venv/Scripts/activate.bat
 rem pip install -r requirements.txt
+virtualenv .venv && source .venv/Scripts/activate && pip install -r requirements.txt
 pytest -v -s tests/test_login.py
 
 rem pytest -v -s tests/test_adminpage.py
